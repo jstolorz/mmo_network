9450f055a9e55a8e0d103a0e914691f324942afb
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-02-11 13:43:43 +0100

    adding simple client

--- client/main.cpp
+++ client/main.cpp
@@ -4,30 +4,94 @@
 
 #include <iostream>
 
-#include "../framework/net_client.h"
+#include "../framework/bluesoft_net.h"
 
-enum class custom_types : uint32_t {
-    FIRE_BULLET,
-    MOVE_PLAYER
+enum class custom_msg_types : uint32_t {
+    ServerAccept,
+    ServerDeny,
+    ServerPing,
+    MessageAll,
+    ServerMessage,
 };
 
-class custom_client : public bluesoft::net::client_interface<custom_types>{
+class custom_client : public bluesoft::net::client_interface<custom_msg_types> {
 public:
-    bool fire_bullet(float x, float y){
-        bluesoft::net::message<custom_types> msg;
-        msg.header.id = custom_types::FIRE_BULLET;
-        msg << x << y;
-        send(msg);
+    void ping_server() {
+        bluesoft::net::message<custom_msg_types> msg;
+        msg.header.id = custom_msg_types::ServerPing;
 
-        return true;
+        std::chrono::system_clock::time_point time_now = std::chrono::system_clock::now();
+
+        msg << time_now;
+        send(msg);
+    }
+
+    void message_all() {
+        bluesoft::net::message<custom_msg_types> msg;
+        msg.header.id = custom_msg_types::MessageAll;
+        send(msg);
     }
 };
 
-int main(){
+int main() {
 
     custom_client c;
-    c.connect("93.184.216.34",80);
-    c.fire_bullet(2.0f,4.9f);
+    c.connect("127.0.0.1", 60000);
+
+    bool key[3] = { false, false, false };
+    bool old_key[3] = { false, false, false };
+
+    bool b_quit = false;
+
+    while (!b_quit){
+
+        if (GetForegroundWindow() == GetConsoleWindow())
+        {
+            key[0] = GetAsyncKeyState('1') & 0x8000;
+            key[1] = GetAsyncKeyState('2') & 0x8000;
+            key[2] = GetAsyncKeyState('3') & 0x8000;
+        }
+
+        if (key[0] && !old_key[0]) c.ping_server();
+        if (key[1] && !old_key[1]) c.message_all();
+        if (key[2] && !old_key[2]) b_quit = true;
+
+        if(c.is_connected()){
+
+            if(!c.incoming().empty()){
+                auto msg = c.incoming().pop_front().msg;
+
+                switch(msg.header.id){
+                    case custom_msg_types::ServerAccept:{
+                        std::cout << "Server Accepted Connection\n";
+                    }
+                    break;
+
+                    case custom_msg_types::ServerPing:{
+                        std::chrono::system_clock::time_point time_now = std::chrono::system_clock::now();
+                        std::chrono::system_clock::time_point time_then;
+                        msg >> time_then;
+                        std::cout << "Ping: " << std::chrono::duration<double>(time_now - time_then).count() << "\n";
+                    }
+                    break;
+
+                    case custom_msg_types::ServerMessage:{
+                         uint32_t client_id;
+                         msg >> client_id;
+                        std::cout << "Helllo from [" << client_id << "]\n";
+                    }
+
+                    break;
+                }
+
+            }
+
+        }else{
+            std::cout << "Server down\n";
+            b_quit = true;
+        }
+
+    }
 
     return 0;
 }
